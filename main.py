8f0b4d99a387f6bc408e66120f5183bcc164d952
z6daB <patsaifedor08@yandex.ru>
2024-02-21 21:38:17 +0300

    edit page

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def start():
-    return flask.render_template('start.html')
+    return flask.render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=7000)
