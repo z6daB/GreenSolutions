202a8c27f6e4526c2d2f0abe3658544130db9254
z6daB <patsaifedor08@yandex.ru>
2024-03-05 21:50:28 +0300

    edit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from data import db_session
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
 
 @app.route('/')
 def index():
