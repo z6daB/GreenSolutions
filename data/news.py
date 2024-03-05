2cb610f6c4f340ed4ce18df7a4a7f1ba6ccc1183
z6daB <patsaifedor08@yandex.ru>
2024-03-05 22:56:11 +0300

    creating sample for pages and register form

--- data/news.py
+++ data/news.py
@@ -2,7 +2,6 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 
-
 from .db_session import SqlAlchemyBase
 
 
--- data/users.py
+++ data/users.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase):
@@ -15,3 +16,9 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
