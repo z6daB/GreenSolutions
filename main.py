b6b7e907e85c4e49e96b5f150c4cb06f1dc9fe4d
z6daB <patsaifedor08@yandex.ru>
2024-03-05 12:07:12 +0300

    creating a database

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-
+from data import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -14,5 +15,10 @@ def blog():
     return render_template('blog.html')
 
 
-if __name__ == '__main__':
+def main():
+    db_session.global_init('db/blogs.sqlite')
     app.run(debug=True, port=5001)
+
+
+if __name__ == '__main__':
+    main()
