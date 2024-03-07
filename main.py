66ff1e47327e586e77228c257f31d503ea9711ee
z6daB <patsaifedor08@yandex.ru>
2024-03-07 17:11:52 +0300

    edit bg

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import flask
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, make_response
 from data import db_session
 from forms.user import RegisterForm
 from data.users import User
 from sqlalchemy import orm
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -43,6 +42,22 @@ def register():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
 
 
 def main():
